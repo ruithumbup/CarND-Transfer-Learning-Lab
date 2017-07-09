29534fc4b9e2c3dc7b10e327ba0338303f42764c
ruithumbup <rui.li.roy@gmail.com>
2017-07-08 17:18:17 -0700

    new model.fit

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,6 +1,9 @@
 import pickle
 import tensorflow as tf
 # TODO: import Keras layers you need here
+import numpy as np
+from keras.layers import Input, Flatten, Dense
+from keras.models import Model
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -8,7 +11,8 @@ FLAGS = flags.FLAGS
 # command line flags
 flags.DEFINE_string('training_file', '', "Bottleneck features training file (.p)")
 flags.DEFINE_string('validation_file', '', "Bottleneck features validation file (.p)")
-
+flags.DEFINE_integer('epochs', 50, "The number of epochs.")
+flags.DEFINE_integer('batch_size', 256, "The batch size.")
 
 def load_bottleneck_data(training_file, validation_file):
     """
@@ -46,8 +50,25 @@ def main(_):
     # the dataset
     # 10 for cifar10
     # 43 for traffic
+    # Skip the number of samples
+    nb_classes = len(np.unique(y_train))
 
-    # TODO: train your model here
+    # Define model
+    input_shape = X_train.shape[1:]
+    model = Sequential()
+    model.add(Flatten(input_shape))
+    model.add(Dense(nb_classes, activation='softmax'))
+
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    # Train model
+    model.fit(
+        X_train, 
+        y_train, 
+        epochs=FLAGS.epochs, 
+        batch_size=FLAGS.batch_size, 
+        validation_data=(X_val, y_val), 
+        shuffle=True)
 
 
 # parses flags and calls the `main` function above
